b6726e86c6d09992753d63d7d90ec4f993499d2c
Veysel Aydın ERALP <veyselaydineralp@gmail.com>
2024-08-13 10:03:52 +0300

    fibonacci

---
+++ fibonacci/fibonacci/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir sayı girin: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Fibonacci Serisi:");
+
+            int a = 0;
+            int b = 1;
+            int c;
+
+            Console.Write($"{a} {b} ");
+
+            for (int i = 2; i < n; i++)
+            {
+                c = a + b;
+                Console.Write($"{c} ");
+                a = b;
+                b = c;
+            }
+
+            Console.WriteLine(); // Yeni satır ekler.
+        }
+    }
+}
